30a3fd4b94edad650d8dbdcbcdc8f6ee83654971
Marcelo Javier Valdez <110573419+Xavier3rd@users.noreply.github.com>
2024-04-03 19:57:23 -0300

    Estamos probando el JSON

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+const formulario = document.getElementById('formulario')
 
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    fetch('https://sheet.best/api/sheets/e296ffed-8b51-411a-b60b-ad8af4dd04f4', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+            'Nombre Completo': 'Ejemplo de nombre',
+            'Fecha de Nacimiento': 'Ejemplo de Fecha',
+            'Email': 'Ejemplo de Email',
+            'Foto': 'Ejemplo de Foto'
+        })
+    });
+})
